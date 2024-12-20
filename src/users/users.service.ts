27bf7dee2307598fefaa5137ea550831072c90e8
Tareq-Adel <tareq212adel@gmail.com>
2024-12-20 17:51:58 +0200

    Create and save users to mongoose

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
+import { CreateUserDto } from './dto/createUser.dto';
+
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  //DTO Data Transfer Object
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = new this.userModel(createUserDto);
+    return newUser.save();
+  }
 }
