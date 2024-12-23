e7c235259bb3098850ee7a2c17c5681c85dce30d
Tareq-Adel <tareq212adel@gmail.com>
2024-12-23 17:29:12 +0200

    New couple of routs (git user by id and update user fields with specific id using 'Patch rout')

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,18 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/createUser.dto';
+import mongoose from 'mongoose';
+import { UpdateUserDto } from './dto/upsateUser.dto';
 
 @Controller('users')
 export class UsersController {
@@ -23,5 +29,36 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    // if (!mongoose.isValidObjectId(id)) it is also right
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+    const findUser = await this.usersService.getUserById(id);
+
+    console.log(findUser);
+    if (!findUser)
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+    return findUser;
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe())
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+    const updatedUser = await this.usersService.updateUser(id, updateUserDto);
+
+    if (!updatedUser)
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    return updatedUser;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/upsateUser.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,5 +16,15 @@ export class UsersService {
 
   getUsers() {
     return this.userModel.find();
+  }
+
+  getUserById(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  updateUser(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findOneAndUpdate({ _id: id }, updateUserDto, {
+      new: true,
+    });
   }
 }
